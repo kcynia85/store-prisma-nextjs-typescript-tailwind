aeb9be67922f300d8aa1a89f5813aa7afac707c2
Kamil Lazarski <lazarski.k.s@gmail.com>
2024-04-13 16:39:02 +0200

    Enhance admin dashboard with real-time statistics and added currency/number formatting support
    Enhance admin dashboard with real-time sales, user, and product
    statistics and added formatting for currency and numbers

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -4,46 +4,96 @@ import {
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/Card";
-
+} from "@/components/ui/Card"
+import db from "@/db/db"
+import { formatCurrency, formatNumber } from "@/lib/formatters"
 
 const getSalesData = async () => {
-  // Call API and return data
+  const data = await db.order.aggregate({
+    _sum: { pricePaidInCents: true },
+    _count: true,
+  })
+
+  return {
+    amount: (data._sum.pricePaidInCents || 0) / 100,
+    numberOfSales: data._count,
+  }
 };
 
-const AdminDashboard = () => {
+const getUserData = async () => {
+  const [userCount, orderData] = await Promise.all([
+    db.user.count(),
+    db.order.aggregate({
+      _sum: { pricePaidInCents: true },
+    }),
+  ])
+
+  return {
+    userCount,
+    averageValuePerUser:
+      userCount === 0
+        ? 0
+        : (orderData._sum.pricePaidInCents || 0) / userCount / 100,
+  }
+}
+
+const getProductData = async () => {
+  const [activeCount, inactiveCount] = await Promise.all([
+    db.product.count({ where: { isAvailableForPurchase: true } }),
+    db.product.count({ where: { isAvailableForPurchase: false } }),
+  ])
+
+  return { activeCount, inactiveCount }
+}
+
+const AdminDashboard = async () => {
+  const [salesData, userData, productData] = await Promise.all([
+    getSalesData(),
+    getUserData(),
+    getProductData(),
+  ])
+
   return (
-    <div className="
-      grid grid-cols-1 
-      md:grid-cols-2 
-      lg:grid-cols-3 
-      gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       <DashboardCard
-        title="Orders"
-        subtitle="Last 24 Hours"
-        body="345"/>
+        title="Sales"
+        subtitle={`${formatNumber(salesData.numberOfSales)} Orders`}
+        body={formatCurrency(salesData.amount)}
+      />
+      <DashboardCard
+        title="Customers"
+        subtitle={`${formatCurrency(
+          userData.averageValuePerUser
+        )} Average Value`}
+        body={formatNumber(userData.userCount)}
+      />
+      <DashboardCard
+        title="Active Products"
+        subtitle={`${formatNumber(productData.inactiveCount)} Inactive`}
+        body={formatNumber(productData.activeCount)}
+      />
     </div>
-  );
-};
+  )
+}
 
 type DashboardCardProps = {
-  title: string;
-  subtitle: string;
-  body: string;
-};
+  title: string
+  subtitle: string
+  body: string
+}
 
-const DashboardCard = ({title, subtitle, body}: DashboardCardProps) => {
-  return  (
+const DashboardCard = ({ title, subtitle, body }: DashboardCardProps) => {
+  return (
     <Card>
-    <CardHeader>
-      <CardTitle>{title}</CardTitle>
-      <CardDescription>{subtitle}</CardDescription>
-    </CardHeader>
-    <CardContent>
-      <p>{body}</p>
-    </CardContent>
-  </Card>
-  );
-};
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{subtitle}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{body}</p>
+      </CardContent>
+    </Card>
+  )
+}
 
 export default AdminDashboard;
